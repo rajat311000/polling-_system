290d8ea1c475eb5140296421c3ec094d30392aa1
rajat311000 <130726733+rajat311000@users.noreply.github.com>
2023-07-31 02:47:15 +0530

    Update options.js

--- routes/api/v1/options.js
+++ routes/api/v1/options.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const optionsController = require('../../../controllers/api/v1/option_controllers');
+const homeController = require('../../../controllers/api/v1/home_controller');
+router.get('/', homeController.homePage);
 
 router.delete('/:id/delete', optionsController.delete); // delete option 
 router.get('/:id/add_vote', optionsController.addVote); // add vote to the option
