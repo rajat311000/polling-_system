696dd20cfeb0ba815fc86a9fbb9fe908e2bf38e3
rajat311000 <130726733+rajat311000@users.noreply.github.com>
2023-07-31 19:50:22 +0530

    Update options.js

--- routes/api/v1/options.js
+++ routes/api/v1/options.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 const optionsController = require('../../../controllers/api/v1/option_controllers');
 
 router.delete('/:id/delete', optionsController.delete); // delete option 
